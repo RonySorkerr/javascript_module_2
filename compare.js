8abab20db41075f57337a2ba7f6aa38c02a8bb51
RonySorkerr <mdronysarkar33299@gmail.com>
2024-01-26 21:00:10 +0600

    compare done

--- compare.js
+++ compare.js
@@ -1,1 +1,21 @@
-console.log(6>7);
+// comparison 
+// console.log(6>7);
+
+// console.log(10 <= 10.5);
+
+// //------------------------------------------------
+// var payaraPrice = 100;
+// var applePrice = 250;
+// console.log(payaraPrice > applePrice);
+// console.log(applePrice > payaraPrice);
+// console.log(applePrice <= payaraPrice);
+// console.log(applePrice >= payaraPrice);
+
+// double == vs === equal
+// console.log(10 == '10'); //if there is any number stored in a var with the value of numeric but in quotes it will consider it as a number
+// console.log(10 === '10'); //TRUE // if there is any number is stored in a var with the value of numeric but in qoutes it will consider as string and the result will be false. Cause it checks the type.
+
+// -------------
+console.log(10 != '10');
+console.log(10 !== '10');
+
