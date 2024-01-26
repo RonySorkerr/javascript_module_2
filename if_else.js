702457fe24e5bca9d81290f8c61a732bd2b1d8ee
RonySorkerr <mdronysarkar33299@gmail.com>
2024-01-26 21:26:22 +0600

    if else done

--- if_else.js
+++ if_else.js
@@ -1,2 +1,22 @@
 //-------------------------
-if (3 > 10);
+if (3 > 10);{
+    console.log('You are totally drunk');
+}
+
+var mamarBari = 120;
+if (mamarBari <= 120){
+    console.log('ami jamo mama bari');
+}
+
+/*
+
+*/
+
+var weight =  55;
+
+if (weight < 35){
+    console.log('I will have you in my kole.')
+}
+else{
+    console.log('Bapre ami nai')
+}  
