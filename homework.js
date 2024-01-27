8adf57d574a32fe39fb35c20eca64d15b40c4568
RonySorkerr <mdronysarkar33299@gmail.com>
2024-01-27 12:52:04 +0600

    ternary done

--- homework.js
+++ homework.js
@@ -71,3 +71,17 @@ if(myMarks >= 80){
 else{
     console.log('I am home and sad');
 }
+
+// TERNARY 
+const num1 = 12;
+const num2 = 13;
+var result = num1 + num2;
+if(num1 > num2){
+    console.log(num1 * 2);
+}
+else{
+    console.log(result);
+}
+
+result =  num1 > num2 ? num1 * 2 : result;
+console.log(result);
