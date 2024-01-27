4e02196f5c95278589fc35954194313a794419dc
RonySorkerr <mdronysarkar33299@gmail.com>
2024-01-27 09:15:14 +0600

    grade system done

--- Mlevel.js
+++ Mlevel.js
@@ -1,9 +1,39 @@
-// multi level conditions
+// Multi_level Condition
+const price = 7500;
+if(price <= 7000 && price == 7500){
+    // 5% discount
+    const discout = price * 5 / 100;
+    console.log('Your discount is : ' , discout);
+    const payAmount = price - discout;
+    console.log('Amazing ! You paying amount is only : ', payAmount);
+}
+else if(price > 7000){
+    const discount = price * 6 / 100;
+    console.log(discount);
+    const totalPrice = price -  discount;
+    console.log(totalPrice);
+}
+else{
+    console.log('You have to pay : ', price);
+}
 
-const price = 50000;
-if(price >= 5000){
-    //10% discount
-    const discount = price * 10 / 100;
-    const payAmoutn = price - discount ;
-    console.log(payAmoutn);
+const Price = 2500;
+const age =  55;
+if(age <= 21){
+    console.log('You can eat here for free');
+}
+else if(age <= 30){
+    // 6% discout 
+    const discount = Price * 6 / 100;
+    const yourBill = Price -  discount;
+    console.log(yourBill);
+}
+
+else if(age > 50){
+    const discount = Price * 2 / 100;
+    const price =  Price -  discount;
+    console.log('Your bill is : ', price);
+}
+else{
+    console.log('Your bill is : ', Price);
 }
---
+++ yourResult.js
@@ -0,0 +1,22 @@
+const total = 500;
+if(total >= 550){
+    console.log('A+');
+}
+else if(total >= 500){
+    console.log('A');
+}
+else if(total >= 450){
+    console.log('B');
+}
+else if(total >= 400){
+    console.log('B-');
+}
+else if(total >= 380){
+    console.log('B-');
+}
+else if(total >= 350){
+    console.log('C');
+}
+else{
+    console.log('You are failed');
+}
